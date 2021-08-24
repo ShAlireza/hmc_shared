5156e6900dd713ac42035c22a1be71a6f1cc1a0d
AlirezaShateri <alirezashateri7@gmail.com>
2021-08-25 02:44:51 +0430

    Add retry policy to request method

--- hmc_shared/utils/http.py
+++ hmc_shared/utils/http.py
@@ -9,19 +9,27 @@ def request(
         json: Dict[str, Any] = None,
         headers: Dict[str, Union[str, int, float]] = None,
         cookies: Dict[str, Union[str, int, float]] = None,
+        retry_count: int = 0
 ) -> Tuple[Dict[str, Any], int]:
     params = {} if not params else params
     json = {} if not json else json
 
-    response = requests.request(
+    assert retry_count >= 0, "retry_count must be non-negative"
+
+    data = None
+    while retry_count > -1:
+        response = requests.request(
             method=method,
             url=url,
             params=params,
             json=json,
             headers=headers,
             cookies=cookies
-    )
-    data = response.json()
-    status_code = response.status_code
+        )
+        status_code = response.status_code
+        retry_count -= 1
+        if 200 <= status_code < 300:
+            data = response.json()
+            break
 
     return data, status_code
