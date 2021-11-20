cf4e14402fca10f89a05f67dadf548b8a8b77996
AlirezaShateri <alirezashateri7@gmail.com>
2021-11-21 00:45:51 +0330

    Add proxies field to request

--- hmc_shared/utils/http.py
+++ hmc_shared/utils/http.py
@@ -9,7 +9,8 @@ def request(
         json: Dict[str, Any] = None,
         headers: Dict[str, Union[str, int, float]] = None,
         cookies: Dict[str, Union[str, int, float]] = None,
-        retry_count: int = 0
+        retry_count: int = 0,
+        proxies: Dict[str, str] = None
 ) -> requests.Response:
     params = {} if not params else params
     json = {} if not json else json
@@ -24,7 +25,8 @@ def request(
             params=params,
             json=json,
             headers=headers,
-            cookies=cookies
+            cookies=cookies,
+            proxies=proxies
         )
         status_code = response.status_code
         retry_count -= 1
